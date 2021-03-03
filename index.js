f9d8c3068bc4fdecda3a7fafaa193a71f410bd60
229876 Fronc Sergiusz <229876@edu.p.lodz.pl>
2021-03-03 15:42:59 +0100

    Removed bot commands

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const PREFIX = '!';
-
 client.on('ready', () => {
-    console.log('This bot is online!');
+    console.log('Bot is online!');
 
     const channel = client.channels.cache.get(process.env.channel_id);
 
@@ -12,27 +10,13 @@ client.on('ready', () => {
         return console.error("The channel does not exist!");
     }
 
-    channel.join().then(connection => {
+    channel.join().then(() => {
       // Yay, it worked!
-      console.log("Successfully connected.");
+      console.log("Successfully connected!");
     }).catch(e => {
       // Oh no, it errored! Let's log it to console :)
       console.error(e);
     });
-});
-
-client.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]) {
-        case 'laguna':
-            message.reply('brum brum')
-            break;
-    }
-
-    if (message.content === "odpalam lagune") {
-        message.reply('brum brum');
-    }
 });
 
 client.login(process.env.token);
